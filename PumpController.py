8ddcf11143e7887ceaca552511a96d084dc124c7
milan285 <milan285@users.noreply.github.com>
2016-07-16 19:08:36 +0200

    Update PumpController.py

--- PumpController.py
+++ PumpController.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python
 import os
 import sys
-#script must be run as root
-if not os.getegid() == 0:
-    sys.exit('Script must be run as root')
-
-watering_wait == 259200 #it waits 3 days after watering before asking for signal again
-check_wait == 7200 #checks for arduino signal every 2 hours
-pump_on == 5 #turns on the pump for this long
-
 from time import sleep
 from pyA20.gpio import gpio
 from pyA20.gpio import port
@@ -16,6 +8,14 @@ from pyA20.gpio import connector
 import logging
 import datetime
 import time
+
+#script must be run as root
+if not os.getegid() == 0:
+    sys.exit('Script must be run as root')
+
+check_wait == 7200 #checks for arduino signal every 2 hours
+pump_on == 4 #turns on the pump for this long (seconds)
+
 #creates a log file
 logging.basicConfig(filename='watering.log',level=logging.DEBUG)
 
@@ -25,10 +25,12 @@ gpio.init()
 logging.info('starting scipt')
 time.sleep(10)
 logging.info('started script')
+
 #sets up gpio
 gpio.setcfg(port.PG6, gpio.OUTPUT)#this pin asks for signal from arduino
 gpio.setcfg(port.PG9, gpio.OUTPUT)#this pin starts the pump
 gpio.setcfg(port.PG8, gpio.INPUT)#this pin reads the signal from arduino
+
 #if nothing is connected input is 0
 gpio.pullup(port.PG8, 0)
 gpio.pullup(port.PG8, gpio.PULLDOWN)
@@ -36,12 +38,14 @@ gpio.pullup(port.PG8, gpio.PULLDOWN)
 while True:
     gpio.output(port.PG6, 1) #asks arduino for the signal
     time.sleep(5) #gives some time for the arduino to respond
+    logging.info("Checking for signal : %s" % time.ctime())
     if gpio.input(port.PG8) == 1:
         gpio.output(port.PG9, 1)
-        logging.info("Watering now : %s" % time.ctime())
+        logging.info("Turned the Pump on : %s" % time.ctime())
         time.sleep(int(pump_on))
+        logging.info("Turned the Pump off : %s" % time.ctime())
         gpio.output(port.PG9, 0)
-        time.sleep(int(watering_wait))
+        time.sleep(int(check_wait))
     else:
         gpio.output(port.PG9, 0)
         logging.info('waiting for arduino signal: %s' % time.ctime())
