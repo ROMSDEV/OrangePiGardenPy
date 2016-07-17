8b91a5f0f7a8fdea018e3aaba78fe9c5d912313b
milan285 <milan285@users.noreply.github.com>
2016-07-17 10:00:34 +0200

    Update Arduinocode.ino

--- Arduinocode.ino
+++ Arduinocode.ino
@@ -1,11 +1,11 @@
 const int VAL_PROBE = 0; // Analog pin 0
-const int MOISTURE_LEVEL = 900; // the value after the LED goes ON
-int opipin = 8;
-int OPIsignal = 0;
+const int MOISTURE_LEVEL = 900; // the value after the pump goes on
+int opipin = 8; //recieves the sinal from orangepi
+int OPIsignal = 0; //
 void setup() {
     Serial.begin(9600);
     pinMode(opipin, INPUT);
-    pinMode(7, OUTPUT);
+    pinMode(7, OUTPUT); //sends the signal to orange pi
     digitalWrite(7, LOW);
 }
  
@@ -13,11 +13,11 @@ void loop() {
     int moisture = analogRead(VAL_PROBE);
     OPIsignal = digitalRead(opipin);
     Serial.println(moisture);
-if (OPIsignal == HIGH) 
+if (OPIsignal == HIGH)  //if there is signal from opi
     if(moisture > MOISTURE_LEVEL) {
-        digitalWrite(7, HIGH);
+        digitalWrite(7, HIGH); //starts sending the signal
         delay(8000);
-        digitalWrite(7, LOW);
+        digitalWrite(7, LOW); //turns the signal off
     } else   {
         digitalWrite(7, LOW);
     }
